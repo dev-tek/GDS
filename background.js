6c437858aa477695c609c1df8c1910cea7a185a7
tek <55714013+dev-tek@users.noreply.github.com>
2020-05-23 15:12:48 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,13 @@
+// Modifications and  design elements Copyright 2020 developer tek  use of this source code is governed under the MIT License 
+// Original source code Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license.
+// Please refer to the LICENSE  file for additional notices.
+// https://opensource.google/docs/
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    // Encode user input for special characters , / ? : @ & = + $ #
+    var newURL = 'https://datasetsearch.research.google.com/search?query=' + encodeURIComponent(text);
+    chrome.tabs.create({ url: newURL });
+  });
