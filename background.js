043a3936261c5ebe2ba72209e63131a9807363ef
tek <55714013+dev-tek@users.noreply.github.com>
2020-05-23 21:10:03 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // Modifications and design elements Copyright 2020 developer tek use of this source code is governed under the MIT License. 
 // Original source code Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license.
-// Please refer to the LICENSE  file for additional notices.
+// Please refer to the LICENSE file for additional notices.
 // https://opensource.google/docs/
 
 // This event is fired with the user accepts the input in the omnibox.
